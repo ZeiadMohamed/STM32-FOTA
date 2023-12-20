63c0fcdd1236cf8550fa0afb6276a0f91c05e22a
ZeiadMohamed <zeiadsalah01@gmail.com>
2023-12-20 14:10:09 +0200

    FOTA Website

---
+++ website/start_flash.php
@@ -0,0 +1,87 @@
+
+<?php
+	if ($_SERVER['REQUEST_METHOD'] === 'POST')
+	{
+	  if( $_POST['flash'] == 'no_file')
+	  {
+
+	  }
+	  else if ( $_POST['flash'] == 'ok')
+	  {
+	      /* Open the file */
+	      $myfile = fopen("lengthPtr.txt","w");
+	      fwrite($myfile,'0');
+	      /* Close the file */
+	      fclose($myfile);
+	      $_POST['flash'] = 'done';
+	      header ("Location: index.html");
+	      //echo "start";
+	  }
+
+	}
+
+  $files = glob('uploads/*'); // get all file names
+  if (is_file($files[0]))
+  {
+      $path = $files[0];
+      $mydata = file($path); //read file in array
+      $length = count($mydata);
+
+      if ( isset( $_GET['config'] ) )
+      {
+        /* Open the file */
+        $myfile = fopen("lengthPtr.txt","r");
+        $ptr =  fgets($myfile);
+        fclose($myfile);
+
+
+        if (( $_GET['config'] == 'ok') && ($ptr < $length) && ($ptr >= 0))
+        {
+            echo $mydata[$ptr];
+            $ptr++ ;
+            $myfile = fopen("lengthPtr.txt","w");
+            fwrite($myfile,$ptr);
+            fclose($myfile);
+            //header ("Location: test.php?config=none");
+        }
+        elseif ( $_GET['config'] == 'none' && ($ptr < $length) && ($ptr >= 0))
+        {
+            //echo ($ptr-1);
+            echo $mydata[$ptr-1];
+
+        }
+        elseif (($ptr == $length) || ($_GET['config'] == 'doneBurn'))
+        {
+            echo "DONE UPLOAD ";
+            /* Open the file */
+            $myfile = fopen("lengthPtr.txt","w");
+            fwrite($myfile,'-1');
+            /* Close the file */
+            fclose($myfile);
+
+            if(is_file($files[0]))
+            {
+              unlink($files[0]); // delete file
+            }
+        }
+
+      }
+			else
+			{
+				// config Not set...
+				$myfile = fopen("lengthPtr.txt","w");
+				fwrite($myfile,'0');
+				/* Close the file */
+				fclose($myfile);
+			}
+  }
+  else
+  {
+      /* No files Found */
+           echo "NO FILE FOUND "."%";
+     
+
+  }
+
+
+?>
---
+++ website/upload.php
@@ -0,0 +1,41 @@
+<?php
+
+
+
+
+
+    $errors= array();
+    $file_name = $_FILES['fileToUpload']['name'];
+    $file_size = $_FILES['fileToUpload']['size'];
+    $file_tmp = $_FILES['fileToUpload']['tmp_name'];
+    $file_type = $_FILES['fileToUpload']['type'];
+    $file_ext=strtolower(end(explode('.',$_FILES['fileToUpload']['name'])));
+
+    $expensions= array("hex");
+
+
+    if(in_array($file_ext,$expensions)=== false)
+    {
+      $errors[]="extension not allowed, please choose a HEX file.<br>";
+    }
+
+    if($file_size > 500000)
+    {
+      $errors[]='File size must be excately 500kb';
+    }
+
+    if(empty($errors)==true)
+    {
+      move_uploaded_file($file_tmp,"uploads/".$file_name);
+      echo "Success";
+      echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";;
+
+    header ("Location: done_upload.html" );
+    }
+    else
+    {
+      echo "Sorry, there was an error uploading your file.";
+      echo "<br>";
+      print_r($errors);
+    }
+?>
